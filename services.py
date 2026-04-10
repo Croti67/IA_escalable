837a4e2458ec5179ca674d5c5f98add468680a61
Croti <croti67@outlook.com>
2026-04-10 16:29:17 -0500

    Add files via upload

---
+++ services.py
@@ -0,0 +1,7 @@
+# services.py
+
+def get_customer(customer_id):
+    return f"Cliente {customer_id}: Juan Pérez, saldo $500"
+
+def get_tickets(customer_id):
+    return f"Cliente {customer_id} tiene 2 tickets abiertos"
