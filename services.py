2a55a535969cf73a3415c720ee79c8072936ca03
Croti <croti67@outlook.com>
2026-04-14 15:27:10 -0500

    Delete services.py

--- services.py
+++
@@ -1,7 +0,0 @@
-# services.py
-
-def get_customer(customer_id):
-    return f"Cliente {customer_id}: Juan Pérez, saldo $500"
-
-def get_tickets(customer_id):
-    return f"Cliente {customer_id} tiene 2 tickets abiertos"
